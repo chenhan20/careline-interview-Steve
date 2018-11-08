6dd79dbf1135501a3d3e82ad2b1994e8a7b08f59
SteveChuang <a0911558945@gmail.com>
2018-11-09 02:27:07 +0800

    commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO `tmp` (`key`, `value`, `desc`) VALUES ('System.Start', NOW(), 'System start time.');
+INSERT INTO `h_user` (`id`,`email`, `name`, `password`) VALUES ('123', '123asd', 'asd','asd');
